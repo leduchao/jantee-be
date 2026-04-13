b8c93b0201c5e5a5c93cfeebd09572ef82d5f4f0
Hao Le <leduchao77g1@gmail.com>
2026-04-13 11:09:47 +0700

    update sign up

--- src/Modules/Users/Jantee.Modules.Users.Presentation/Endpoints/SignUp/SignUp.cs
+++ src/Modules/Users/Jantee.Modules.Users.Presentation/Endpoints/SignUp/SignUp.cs
@@ -5,15 +5,18 @@ using Microsoft.AspNetCore.Routing;
 
 namespace Jantee.Modules.Users.Presentation.Endpoints.SignUp;
 
-public record SignUpRequest(string Email, string Password);
+public record Request(string Email, string Password);
+public record Response(string Username, string? Fullname, DateTime CreatedAt);
 
 public class SignUpEndpoint : IMinimalEndpoint
 {
     public IEndpointRouteBuilder MapEndpoint(IEndpointRouteBuilder builder)
     {
-        builder.MapGet("/sign-up", () =>
+        builder.MapPost($"{PresentationConstants.BaseApiEndpoint}/sign-up", (Request request) =>
         {
-            return Results.Ok("Register user succeeded");
+            var name = request.Email.Split('@')[0];
+            var response = new Response(name, name, DateTime.UtcNow);
+            return Results.Ok(new { message = "Register user succeeded", data = response });
         })
         .WithName("SignUp");
 
---
+++ src/Modules/Users/Jantee.Modules.Users.Presentation/PresentationConstants.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Jantee.Modules.Users.Presentation;
+
+public static class PresentationConstants
+{
+    private const string ApiVersion = "1";
+    public const string BaseApiEndpoint = $"/api/v{ApiVersion}";
+}
